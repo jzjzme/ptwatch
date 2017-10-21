5c81aa6556947c436289f558bdf7ab68c1f441bd
Dave McCarthy <dave@polleverywhere.com>
2017-10-21 12:31:46 -0700

    more styles

--- src/components/PillButton/index.js
+++ src/components/PillButton/index.js
@@ -5,8 +5,8 @@ import styles from "./styles.scss"
 
 export class PillButton extends Component {
   render () {
-    const { className, children, to, hollow, onClick } = this.props
-    const buttonClassName = `${styles.button} ${hollow && styles.mHollow}`
+    const { className, children, to, hollow, onClick, capitalOne } = this.props
+    const buttonClassName = `${styles.button} ${hollow && styles.mHollow} ${capitalOne && styles.mCapitalOne}`
 
     if (to) {
       return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from './Home/Home'
 export { default as Flights } from './Flights/Flights'
-export { default as Login } from './Login'
+export { default as Login } from './Login/index'
 export { default as PointsDashboard } from './PointsDashboard'
