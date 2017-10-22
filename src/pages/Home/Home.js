046df30323373456348b4b7cc4a3bc9b7775f2df
Dave McCarthy <dave@polleverywhere.com>
2017-10-22 05:06:16 -0700

    add keyline

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,6 +12,7 @@ const Home = observer(({ store: { user, flight }, history }) => (
         <h1 className={styles.header}>Leveraging Decentralized Credit Card Points for Flights and Rewards</h1>
         <h2 className={styles.subHeader}>Monetize your unused credit card points with PointWatch</h2>
       </div>
+      <div className={styles.keyline} />
       <div className={styles.tabs}>
         <div className={`${styles.tab} ${styles.sActive}`}>Flights</div>
         <div className={styles.tab}>Hotels</div>
