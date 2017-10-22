eb954e7e111e3631bdf0c48ea308b6287bf98d53
Dave McCarthy <dave@polleverywhere.com>
2017-10-21 22:26:29 -0700

    pre-selecting a flight

--- src/pages/Flights/index.js
+++ src/pages/Flights/index.js
@@ -5,50 +5,52 @@ import { Link } from "react-router-dom"
 import moment from "moment"
 import styles from "./styles.scss"
 
-const Flights = observer(({ store: { flight: { searchResults } } }) => (
-  <Page>
-    <div className={styles.searchHeader}>
-      <CardGroup>
-        <h1 className={styles.header}>Search</h1>
-        <div className={styles.keyline} />
-        <div className={styles.searchDetails}>
-          <div className={styles.data}>
-            <label>Flights to</label>
-            <div>Singapore Changi Airport</div>
+const Flights = observer(({ store: { flight: { searchResults } } }) => {
+  return (
+    <Page>
+      <div className={styles.searchHeader}>
+        <CardGroup>
+          <h1 className={styles.header}>Search</h1>
+          <div className={styles.keyline} />
+          <div className={styles.searchDetails}>
+            <div className={styles.data}>
+              <label>Flights to</label>
+              <div>Singapore Changi Airport</div>
+            </div>
+            <div className={styles.data}>
+              <label>On</label>
+              <div>{moment("2017-10-25T22:35-07:00").format("MMMM Do, YYYY")}</div>
+            </div>
           </div>
-          <div className={styles.data}>
-            <label>On</label>
-            <div>{moment("2017-10-25T22:35-07:00").format("MMMM Do, YYYY")}</div>
-          </div>
-        </div>
-      </CardGroup>
-    </div>
+        </CardGroup>
+      </div>
 
-    <CardGroup>
-      <Card className={styles.flightContainer}>
-        {searchResults.map(({ arrives_at, departs_at, date, price, id }) => (
-          <Link key={id} className={styles.flight} to={`/flights/${id}`}>
-            <div className={styles.data}>
-              <label>Date</label>
-              <div>{moment(departs_at).format("MMMM Do, YYYY")}</div>
+      <CardGroup>
+        <Card className={styles.flightContainer}>
+          {searchResults.map(({ arrives_at, departs_at, date, price, id }) => (
+            <div key={id} className={styles.flight} onClick={() => this.selectFlight(id)}>
+              <div className={styles.data}>
+                <label>Date</label>
+                <div>{moment(departs_at).format("MMMM Do, YYYY")}</div>
+              </div>
+              <div className={styles.data}>
+                <label>Departs At</label>
+                <div>{moment(departs_at).format('LT')}</div>
+              </div>
+              <div className={styles.data}>
+                <label>Arrives At</label>
+                <div>{moment(arrives_at).format('LT')}</div>
+              </div>
+              <div className={styles.data}>
+                <label>Price</label>
+                <div>{price}</div>
+              </div>
             </div>
-            <div className={styles.data}>
-              <label>Departs At</label>
-              <div>{moment(departs_at).format('LT')}</div>
-            </div>
-            <div className={styles.data}>
-              <label>Arrives At</label>
-              <div>{moment(arrives_at).format('LT')}</div>
-            </div>
-            <div className={styles.data}>
-              <label>Price</label>
-              <div>{price}</div>
-            </div>
-          </Link>
-        ))}
-      </Card>
-    </CardGroup>
-  </Page>
-))
+          ))}
+        </Card>
+      </CardGroup>
+    </Page>
+  )}
+)
 
 export default inject('store')(Flights)
